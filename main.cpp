3d2600a37220395832d5667b2ce3505ddd166bb4
Ved Mitra Verma <vedmitraverma24@gmail.com>
2025-11-13 02:43:52 +0530

    adding main.cpp to control everything

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "CreateDataset.h"
+#include "Graph.h"
+
+const string filePath="Dataset\\facebook_combined(UG).txt";
+
+int main()
+{
+    CreateData data(filePath);//creating dataset
+
+    Graph sniegraph;
+    sniegraph.detectCommunities();//detecting communities
+    sniegraph.betweennessCentrality();//calculating betweenesscentrality
+}
