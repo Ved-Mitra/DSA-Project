6442e3f2a17456d60e536c85108e899e26069833
Darsh-xye <darshchhatrala87@gmail.com>
2025-11-14 02:38:50 +0530

    removed namespace std in header file as it was causing an issue

--- Constant.h
+++ Constant.h
@@ -1,15 +1,13 @@
 #pragma once
 
 #include <bits/stdc++.h>
-using namespace std;
 
-
-const string EDGESFILEPATH= "Dataset/edges.csv";
-const string NODESFILEPATH= "Dataset/nodes.csv";
-const string COMMUNITIES="communities.csv";
-const string BETWEENNESS_CENTRALITY="centrality.csv";
-const string RECOMMEDATION="recommedation.csv";
-const string BRIDGE_RECOMMEDATION="bridgeRecommedation.csv";
+const std::string EDGESFILEPATH= "Dataset/edges.csv";
+const std::string NODESFILEPATH= "Dataset/nodes.csv";
+const std::string COMMUNITIES="communities.csv";
+const std::string BETWEENNESS_CENTRALITY="centrality.csv";
+const std::string RECOMMEDATION="recommedation.csv";
+const std::string BRIDGE_RECOMMEDATION="bridgeRecommedation.csv";
 
 //to be decided
 const double ALPHA=0.5;
@@ -18,11 +16,11 @@ const double GAMMA=0.7;
 const double DELTA=0.6;
 
 //color ANSI code for terminal printing
-const string RESET = "\033[0m";//white color 
-const string RED = "\033[31m";
-const string GREEN = "\033[32m";
-const string YELLOW = "\033[33m";
-const string CYAN = "\033[36m";
+const std::string RESET = "\033[0m";//white color 
+const std::string RED = "\033[31m";
+const std::string GREEN = "\033[32m";
+const std::string YELLOW = "\033[33m";
+const std::string CYAN = "\033[36m";
 
 //for undirected or directed graph
 const bool UNDIRECTED_GRAPH=true;
